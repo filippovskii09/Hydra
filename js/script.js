d87738080aaba177777f787b14bdc7eca6e28bff
filippovskiy09 <arturfilippovskij@gmail.com>
2023-09-15 17:47:23 +0300

    main section ready

--- js/script.js
+++ js/script.js
@@ -61,4 +61,33 @@ window.addEventListener('DOMContentLoaded', () => {
 	mediaQuery.addListener(handleMediaChange);
 	handleMediaChange(mediaQuery);
 
+
+
+	const paralax = (section) => {
+		const elements = document.querySelectorAll('[data-mouse]')
+
+		section.addEventListener('mousemove', (event) => {
+			let { width, height } = section.getBoundingClientRect();
+			let offX = event.pageX - (width * 0.5)
+			let offY = event.pageY - (height * 0.5)
+	
+			// const wrapper = document.querySelector('.main__decors');
+	
+			elements.forEach(element => {
+				const speed = element.getAttribute('data-speed');
+				const x = (offX * speed) / 100;
+				const y = (offY * speed) /100;
+				
+				element.style.transform = `translateX(${x}px) translateY(${y}px)`
+			})
+		})
+
+		// section.addEventListener('mouseleave', () => {
+		// 	elements.forEach(element => {
+		// 		element.style.transform = `translateX(0px) translateY(0px)`
+		// 	})
+		// })
+	}
+	
+	paralax(document.querySelector('.main'));
 })
